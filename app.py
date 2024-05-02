5cfeb2d575ffd508fb00412a29972837934501f7
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-03 02:31:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,10 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     remove_newlines = st.checkbox("Newlines")
-
 with col2:
     remove_tabs = st.checkbox("Tabs")
-
 with col3:
     remove_leading_trailing_spaces = st.checkbox("Lead/Trail Spaces")
-
 with col4:
     remove_all_spaces = st.checkbox("All Spaces")
 
