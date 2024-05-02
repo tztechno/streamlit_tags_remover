d8318b6778173c45784e3338e4e228c6686baf0d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-03 02:28:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,33 @@ import streamlit as st
 st.title('Tags Remover')
 
 text_area_key = "input_text_area"
-text = st.text_area("Input text", key=text_area_key)
+text = st.text_area("Input", key=text_area_key)
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    remove_newlines = st.checkbox("Newlines")
+
+with col2:
+    remove_tabs = st.checkbox("Tabs")
+
+with col3:
+    remove_leading_trailing_spaces = st.checkbox("Lead/Trail Spaces")
+
+with col4:
+    remove_all_spaces = st.checkbox("All Spaces")
 
 def remover(text):
-    # 改行を削除
-    text = text.replace("\n", "")
-    # タブを削除
-    text = text.replace("\t", "")
-    # 行頭・行末のスペースを削除
-    text = text.strip()
-    # 全てのスペースを削除
-    text = "".join(text.split())
+    if remove_newlines:
+        text = text.replace("\n", "")
+    if remove_tabs:
+        text = text.replace("\t", "")
+    if remove_leading_trailing_spaces:
+        text = text.strip()
+    if remove_all_spaces:
+        text = "".join(text.split())
     return text
 
-if st.button("Remove Tags"):
-    text2 = remover(text)
-    st.write(f"Result: {text2}")
+if st.button("Remove"):
+    result = remover(text)
+    st.text_area("Result", value=result)
